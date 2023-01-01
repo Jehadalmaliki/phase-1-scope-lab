2a3cc99882afd55bfb831e87117c979c1f9888e9
Jehad Almaliki <yourgitemail@example.com>
2023-01-01 20:41:15 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+
+  window.bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'john';
+  leastFavoriteCustomer = 'mary';
+  console.log(leastFavoriteCustomer);
+}
